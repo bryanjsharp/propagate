63f4fe1c1590b3df0e088a9e29684a9bd60d090a
bryanjsharp <nope@nope.com>
2016-07-06 22:10:44 -0500

    torrent functionality added

--- client.py
+++ client.py
@@ -1,19 +1,20 @@
 #! /usr/bin/env python
 
 import socket
+import libtorrent as lt
+import time
 
 BUFF = 1024
+host = "127.0.0.1"
+port = 9999
+torrentName = "recv.torrent"
 
-if __name__ == "__main__":
-    s = socket.socket()
-    host = "127.0.0.1"
-    port = 9999
-    s.connect((host, port))
-    with open("recv.torrent", 'wb') as f:
+def receive_torrent_file():
+    with open(torrentName, 'wb') as f:
         print 'file opened'
         print('receiving data...')
         while True:
-            data = s.recv(BUFF)
+            data = sock.recv(BUFF)
             print('data=%s', (data))
             if not data:
                 break
@@ -21,5 +22,34 @@ if __name__ == "__main__":
             f.write(data)
     f.close()
     print "Success!"
-    s.close()
+
+def receive_content():
+    ses = lt.session()
+    ses.listen_on(6881, 6891)
+    e = lt.bdecode(open(torrentName, 'rb').read())
+    info = lt.torrent_info(e)
+
+    params = { 'save_path': '.', \
+        'storage_mode': lt.storage_mode_t.storage_mode_sparse, \
+        'ti': info }
+    h = ses.add_torrent(params)
+
+    stats = h.status()
+    while (not stats.is_seeding):
+        stats = h.status()
+        state_str = ['queued', 'checking', 'downloading metadata', \
+                'downloading', 'finished', 'seeding', 'allocating']
+
+        print '%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
+                (stats.progress * 100, stats.download_rate / 1000, stats.upload_rate / 1000, \
+                stats.num_peers, state_str[stats.state])
+
+        time.sleep(1)
+
+if __name__ == "__main__":
+    sock = socket.socket()
+    sock.connect((host, port))
+    receive_torrent_file()
+    receive_content()
+    sock.close()
     print('connection closed')
